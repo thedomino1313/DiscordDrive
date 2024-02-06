9580b2ab642ee995ce5c3527679325398effa05e
thedomino1313 <dominic.c.beyer@gmail.com>
2024-02-06 17:15:58 -0500

    search upgraded to track any found folders

--- drive.py
+++ drive.py
@@ -12,6 +12,7 @@ class DriveAPI:
     
     folders = dict()
 
+    FOLDER_TYPE = "application/vnd.google-apps.folder"
     SCOPES = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.activity", "https://www.googleapis.com/auth/drive.metadata"]
     def __init__(self, root:str):
         if not root:
@@ -47,7 +48,7 @@ class DriveAPI:
             folder = folder[0]
             self.root = folder
             self.folders[self.root['name']] = self.root['id']
-            print(f"Found folder '{self.root['name']}' with id '{self.root['id']}'")
+            print(f"Found folder '{self.root['name']}' with id '{self.root['id']}' and {self.root}")
         except HttpError as error:
             # TODO(developer) - Handle errors from drive API.
             print(f"An error occurred: {error}")
@@ -56,7 +57,16 @@ class DriveAPI:
         try:
             return self.folders[folder]
         except:
-            return self.search(name=folder, files=False)[0][0]['id']
+            found_folder = self.search(name=folder, files=False)[0][0]
+            if not found_folder:
+                raise HttpError("No folders were found")
+            self.folders[found_folder["name"]] = found_folder["id"]
+            return found_folder["id"]
+
+    def update_folders(self, flist):
+        for file in flist:
+            if file["mimeType"] == self.FOLDER_TYPE:
+                self.folders[file["name"]] = file["id"]
 
     def search(self, name:str='', pageSize:int=1, parent:str='', files=True, folders=True, pageToken:str='', recursive=False):
         l = locals()
@@ -77,25 +87,29 @@ class DriveAPI:
         if files and folders:
             mimeScript = ""
         elif files:
-            mimeScript = "and mimeType!='application/vnd.google-apps.folder'"
+            mimeScript = f"and mimeType!='{self.FOLDER_TYPE}'"
         else:
-            mimeScript = "and mimeType='application/vnd.google-apps.folder'"
-
+            mimeScript = f"and mimeType='{self.FOLDER_TYPE}'"
 
         try:
             results = (
                 self.service.files()
-                .list(pageSize=pageSize, pageToken=pageToken, q=f"trashed = false{mimeScript}{nameScript}{parentScript}", orderBy="folder, name", fields="nextPageToken, files(id, name)")
+                .list(pageSize=pageSize, 
+                      pageToken=pageToken, 
+                      q=f"trashed = false{mimeScript}{nameScript}{parentScript}", 
+                      orderBy="folder, name", 
+                      fields="nextPageToken, files(id, name, mimeType)")
                 .execute()
             )
-
+            files = results.get("files", [])
+            self.update_folders(files)
             if (pageToken := results.get("nextPageToken", "")) != "" and recursive:
                 return results.get("files", []) + self.search(name=name, pageSize=pageSize, parent=parent, files=files, folders=folders, pageToken=pageToken, recursive=recursive)
-            return results.get("files", [])
+            return files
         except HttpError as error:
             print(f"An error occurred: {error}")
             return None, None
 
 if __name__ == "__main__":
     API = DriveAPI("RPI")
-    print(API.search(pageSize=1, parent="RPI"))
+    print(API.search(pageSize=100, parent="RPI"))
