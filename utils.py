7061f46ac9f9db23d55d91f056edb1e2bd51f997
thedomino1313 <dominic.c.beyer@gmail.com>
2024-02-23 16:48:04 -0500

    removed self language from utils because im silly

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import os.path
 
-def empty_dir(self, path):
+def empty_dir(path):
     for file in os.listdir(path):
         if os.path.isdir((newpath := os.path.join(path, file))):
-            self.empty_dir(newpath)
+            empty_dir(newpath)
             os.rmdir(newpath)
         else:
             os.remove(newpath)
