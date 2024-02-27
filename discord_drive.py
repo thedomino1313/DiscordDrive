7d29664792d208c16b93ca2cc0f962b419abdd21
thedomino1313 <dominic.c.beyer@gmail.com>
2024-02-27 15:46:35 -0500

    defaults added to cd suggestions

--- discord_drive.py
+++ discord_drive.py
@@ -113,7 +113,7 @@ class DriveAPICommands(discord.ext.commands.Cog):
         await ctx.respond(f"{DriveAPICommands._wd_cache[ctx.author.id][0]}")
     
     async def _get_folders(ctx: discord.AutocompleteContext):
-        return DriveAPICommands._drive_state[DriveAPICommands._wd_cache[ctx.interaction.user.id][0]]["folders"]
+        return ["~", "..", *DriveAPICommands._drive_state[DriveAPICommands._wd_cache[ctx.interaction.user.id][0]]["folders"]]
 
     @discord.ext.commands.slash_command(name="cd", guild_ids=[os.getenv("DD_GUILD_ID")], description="Change your current working directory")
     async def cd(self, ctx: discord.ApplicationContext, path: discord.Option(str, "Pick a folder", autocomplete=discord.utils.basic_autocomplete(_get_folders))):
