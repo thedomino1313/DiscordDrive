d2e57b8cc0f010db097c3c70d485abadecc07984
ryankarch <karchryan@gmail.com>
2024-04-16 13:08:04 -0400

    Description fix

--- setup.py
+++ setup.py
@@ -19,18 +19,18 @@ description = """
 6. Run the bot, and use `/authenticate` to ensure that DiscordDrive is authorized to access your Google Account.
 
 ## Commands:
-`/authenticate`: Regenerates the token needed to enable the API. If re-authentication is needed, the bot will DM the caller a link and wait for the authentication code given to the caller by Google\
-`/cd <directory>`: Navigates the caller down into a child directory of their current directory. Autocomplete is provided for hints.\
-`/download <file> <timeout (optional)> <public (optional)>`: Gives the user the file (or a link) to download the file specified. Files have autocomplete. Timeout defaults to 60 seconds, where the file will then no longer be allowed to be downloaded. Public defaults to False, where no other users can see the file.\
-`/ls`: Shows the caller the contents of their current directory.\
-`/pwd`: Shows the caller the file path of their current directory.\
-`/share <file> <user> <timeout (optional)>`: Sends a specified server member a dm with a file from the caller's current directory. Files and users have autocomplete. Timeout defaults to 60 seconds, where the file will then no longer be allowed to be downloaded.\
+`/authenticate`: Regenerates the token needed to enable the API. If re-authentication is needed, the bot will DM the caller a link and wait for the authentication code given to the caller by Google\\
+`/cd <directory>`: Navigates the caller down into a child directory of their current directory. Autocomplete is provided for hints.\\
+`/download <file> <timeout (optional)> <public (optional)>`: Gives the user the file (or a link) to download the file specified. Files have autocomplete. Timeout defaults to 60 seconds, where the file will then no longer be allowed to be downloaded. Public defaults to False, where no other users can see the file.\\
+`/ls`: Shows the caller the contents of their current directory.\\
+`/pwd`: Shows the caller the file path of their current directory.\\
+`/share <file> <user> <timeout (optional)>`: Sends a specified server member a dm with a file from the caller's current directory. Files and users have autocomplete. Timeout defaults to 60 seconds, where the file will then no longer be allowed to be downloaded.\\
 `/upload <attachment>`: Uploads a file or zip file to the caller's current directory. Zip files must contain just the files, and no folders, as they will not be read.
 """
 
 setup(
     name='discord_drive',
-    version='0.0.5',
+    version='0.0.6',
     description='Use Google Drive via Discord!',
     license='MIT',
     packages=['discord_drive'],
