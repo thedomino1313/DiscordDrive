c337255dffbae65eff562d71db7a40cbeef12a1c
ryankarch <karchryan@gmail.com>
2024-04-16 12:52:14 -0400

    refactoring and removing unused code

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
     
 setup(
     name='discord_drive',
-    version='0.0.1',
+    version='0.0.5',
     description='Use Google Drive via Discord!',
     license='MIT',
     packages=['discord_drive'],
